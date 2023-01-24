394c1305eee1c1508fe789cc85e222a3c60690c1
dp3why <dp3why@gmail.com>
2023-01-23 19:55:43 -0800

    create func complete

--- router/create.go
+++ router/create.go
@@ -2,7 +2,9 @@ package router
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"mime/multipart"
 	"os"
 
 	"cloud.google.com/go/storage"
@@ -12,9 +14,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-
-var BUCKET_NAME string =  os.Getenv("BUCKET_NAME")
-
 // type createDTO struct {
 // 	Id  string `json:"id" bson:"_id"`
 // 	Title  string `json:"title" bson:"title"`
@@ -22,86 +21,92 @@ var BUCKET_NAME string =  os.Getenv("BUCKET_NAME")
 // 	Year   string `json:"year" bson:"year"`
 // }
 
-func createBook(c *fiber.Ctx) error {
+var BUCKET_NAME string =  os.Getenv("BUCKET_NAME")
 
+func saveToGCS(c *fiber.Ctx, file *multipart.FileHeader) (string, error) {
+    // Open the uploaded file
+    src, err := file.Open()
+    if err != nil {
+        return "", fmt.Errorf("failed to open uploaded file: %w", err)
+    }
+    defer src.Close()
+
+    // Create a client for the Google Cloud Storage API
+    client, err := storage.NewClient(c.Context())
+    if err != nil {
+        return "", fmt.Errorf("failed to create client for Google Cloud Storage: %w", err)
+    }
+
+    // Create/get a bucket
+    bucket := client.Bucket(BUCKET_NAME)
+
+    // Create a new file object
+    obj := bucket.Object(file.Filename)
+
+    // Upload the file to the bucket
+    w := obj.NewWriter(c.Context())
+    if _, err = io.Copy(w, src); err != nil {
+        return "", fmt.Errorf("failed to upload image to Google Cloud Storage: %w", err)
+    }
+
+    if err := w.Close(); err != nil {
+        return "", fmt.Errorf("failed to close writer: %w", err)
+    }
+
+	if err := obj.ACL().Set(c.Context(), storage.AllUsers, storage.RoleReader); err != nil {
+		panic(err)
+ 	}
+    // Get the URL of the uploaded file
+    ctx := context.Background()
+    attrs, _ := obj.Attrs(ctx)
+    return attrs.MediaLink, nil
+}
+
+
+
+// =========== Create ===============
+func createBook(c *fiber.Ctx) error {
     // Get the image from the form
     file, err := c.FormFile("image")
     if err != nil {
         return c.Status(400).JSON(fiber.Map{
-            "error": "Failed to get image from form",
+            "error": "failed to get image from form",
         })
     }
-	// Open the uploaded file
-	src, err := file.Open()
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error": "Failed to open uploaded file",
-		})
-	}
-	defer src.Close()
 
-    // ===== Google GCS:  create a client for the Google Cloud Storage API =====
-    client, err := storage.NewClient(c.Context())
+    // Upload the image to GCS
+    url, err := saveToGCS(c, file)
     if err != nil {
         return c.Status(500).JSON(fiber.Map{
-            "error": "Failed to create client for Google Cloud Storage",
-        })
-    }    
-
-	//======= create / get a bucket =========== 
-	bucket := client.Bucket(BUCKET_NAME)
-
-    // create a new file object
-    obj := bucket.Object(file.Filename)
-
-    // upload the file to the bucket
-    w := obj.NewWriter(c.Context())
-    if _, err = io.Copy(w, src); err != nil {
-        return c.Status(500).JSON(fiber.Map{
-            "error": "Failed to upload image to Google Cloud Storage",
+            "error": err.Error(),
         })
     }
 
-    if err := w.Close(); err != nil {
-        return c.Status(500).JSON(fiber.Map{
-            "error": "Failed to close writer",
-        })
-    }
-
-	ctx := context.Background()
-
- 	attrs, _ := obj.Attrs(ctx)
-	
-
-	id := uuid.New()
-
-    //  ==== Get form values =========
+    // Get form values
     title := c.FormValue("title")
     author := c.FormValue("author")
     year := c.FormValue("year")
 
-	b := bson.M{
-		"_id": id,
+    // Create the book
+    coll := common.GetDBCollection("books")
+    _, err = coll.InsertOne(c.Context(), bson.M{
+        "_id":  uuid.New(),
         "title": title,
         "author": author,
         "year": year,
-		"Url": attrs.MediaLink,
-    } 
+        "Url": url,
+    })
+    if err != nil {
+        return c.Status(500).JSON(fiber.Map{
+            "error":   "Failed to create book",
+            "message": err.Error(),
+        })
+    }
 
-
-	// ===== mongo: create the book ============
-	coll := common.GetDBCollection("books")
-	result, err := coll.InsertOne(c.Context(), b)
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error":   "Failed to create book",
-			"message": err.Error(),
-		})
-	}
-
-	// return the book
-	return c.Status(201).JSON(fiber.Map{
-		"result": result,
-		"Url": attrs.MediaLink,
-	})
+    // Return the book
+    return c.Status(201).JSON(fiber.Map{
+        "result": "Book created successfully",
+        "Url": url,
+    })
 }
+
